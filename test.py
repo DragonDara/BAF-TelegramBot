a3b145e8e734ec21aae7725957132710db4642bc
avokado1337 <holmes1067@gmail.com>
2021-06-06 17:38:22 +0600

    third try

--- test.py
+++ test.py
@@ -30,7 +30,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url='https://aqueous-headland-18855.com/' + TOKEN)
+    bot.set_webhook(url='https://aqueous-headland-18855.herokuapp.com/' + TOKEN)
     return "!", 200
 
 
