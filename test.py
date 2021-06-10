1792143dfe3e31c1df942308d007629a25524f5e
avokado1337 <holmes1067@gmail.com>
2021-06-10 14:49:10 +0600

    try#6

--- test.py
+++ test.py
@@ -13,17 +13,14 @@ server = Flask(__name__)
 def start(message):
     bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
-
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    bot.reply_to(message, message.text)
-
 @bot.message_handler(commands=['getpos'])
 def getpos(message):
     r = requests.get('https://baf-backend.azurewebsites.net/spot/get-open-position?BaseCurrency=DODO&QuoteCurrency=USDT')    
     bot.reply_to(message, str(r.text))
 
-
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
 
 @server.route('/' + TOKEN, methods=['POST'])
 def getMessage():
